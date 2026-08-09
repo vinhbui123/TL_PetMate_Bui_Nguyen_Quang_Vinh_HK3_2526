b92b9ede7274bdb950cc865c58e1b1a88107b3f9
vinh123 <buiquoctuan402@gmail.com>
2026-08-09 20:02:50 +0700

    Fix missing imports in DeviceTokenRepository

--- src/main/java/com/petmate/server/repository/DeviceTokenRepository.java
+++ src/main/java/com/petmate/server/repository/DeviceTokenRepository.java
@@ -1,14 +1,19 @@
 package com.petmate.server.repository;
 
 import com.petmate.server.entity.DeviceToken;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface DeviceTokenRepository extends JpaRepository<DeviceToken, String> {
+
     List<DeviceToken> findByUserId(Long userId);
+
     void deleteByUserId(Long userId);
 
     @Modifying
